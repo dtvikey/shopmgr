3e104c333a78a2193ce3bb30ea4d3e59a93617ad
dtvikey <dtvikey@qq.com>
2019-01-09 17:31:34 +0800

    v1.0

--- src/main/java/com/imooc/shop/repository/UserMapper.java
+++ src/main/java/com/imooc/shop/repository/UserMapper.java
@@ -7,5 +7,6 @@ import org.apache.ibatis.annotations.Select;
  * UserMapper 数据访问类
  */
 public interface UserMapper {
-
+    @Select("select * from ec_user where login_name = #{dfff}")
+    User login(String loginName);
 }
--- src/main/java/com/imooc/shop/service/ShopServiceImpl.java
+++ src/main/java/com/imooc/shop/service/ShopServiceImpl.java
@@ -1,11 +1,16 @@
 package com.imooc.shop.service;
 
 import com.imooc.shop.bean.ArticleType;
+import com.imooc.shop.bean.User;
 import com.imooc.shop.repository.ArticleTypeMapper;
+import com.imooc.shop.repository.UserMapper;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 
 import javax.annotation.Resource;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * @Author: dtvikey
@@ -18,8 +23,45 @@ public class ShopServiceImpl implements ShopService{
     @Resource
     private ArticleTypeMapper articleTypeMapper;
 
+    @Resource
+    private UserMapper userMapper;
+
+
 
     public List<ArticleType> getArticleTypes() {
         return articleTypeMapper.getArticleTypes();
+    }
+
+    @Override
+    public Map<String, Object> login(String loginName, String passWord) {
+        Map<String,Object> results = new HashMap<>();
+        // 1.判断参数是否为空的
+        if(StringUtils.isEmpty(loginName) || StringUtils.isEmpty(passWord)){
+            // 参数为空了
+            results.put("code",1);
+            results.put("msg","参数为空了");
+        }else{
+            // 根据登陆名称去查询用户对象
+            User user =userMapper.login(loginName);
+            if(user !=null){
+                // 判断密码
+                if(user.getPassword().equals(passWord)){
+                    // 登陆成功了
+                    // 应该将登陆成功的用户存入到Session会话中
+                    results.put("code",0);
+                    results.put("msg",user);
+                }else{
+                    // 密码错误了
+                    results.put("code",2);
+                    results.put("msg","密码错误了");
+                }
+            }else{
+                // 登陆名不存在
+                results.put("code",3);
+                results.put("msg","登陆名不存在");
+            }
+
+        }
+        return results;
     }
 }
